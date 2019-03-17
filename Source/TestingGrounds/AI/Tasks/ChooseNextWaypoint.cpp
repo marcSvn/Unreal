3e0730e9d0f3ae7c6abacfa45c0490c4f816ce58
marcSvn <marcSvn@gmail.com>
2019-03-17 10:46:33 +0000

    Adding C++ base class for the third person pawn.

--- Source/TestingGrounds/AI/Tasks/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/AI/Tasks/ChooseNextWaypoint.cpp
@@ -1,13 +1,39 @@
  // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
+#include "Pawns/RoutePatrollerPawn.h"
+#include "AIController.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
     UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
-    auto index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+    AAIController* controller = OwnerComp.GetAIOwner();
+    if (!controller || !BlackboardComp)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Invalid Controller or BlackboardComp!"));
+        return EBTNodeResult::Failed;
+    }
 
-    UE_LOG(LogTemp, Warning, TEXT("AI In c++! %d"), index);
+    ARoutePatrollerPawn* routePatroller = Cast<ARoutePatrollerPawn>(controller->GetPawn());
+    if (!routePatroller)
+    {
+        return EBTNodeResult::Failed;
+    }
+
+    const auto &patrolRoute = routePatroller->PatrolWaypoints;
+
+    auto index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+    if(!ensureMsgf(index >= 0 && index < patrolRoute.Num(), TEXT("Invalid index! %d"), index))
+    {
+        return EBTNodeResult::Failed;
+    }
+
+    AActor *currentWaypoint = patrolRoute[index];
+    BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, currentWaypoint);
+
+    auto nextIndex = (index + 1) % patrolRoute.Num();
+    BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, nextIndex);
+
     return EBTNodeResult::Succeeded;
 }
---
+++ Source/TestingGrounds/Pawns/RoutePatrollerPawn.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TP_ThirdPerson/TP_ThirdPersonCharacter.h"
+#include "RoutePatrollerPawn.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class TESTINGGROUNDS_API ARoutePatrollerPawn : public ATP_ThirdPersonCharacter
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, Category = "Patrol Route")
+    TArray<AActor*> PatrolWaypoints;
+};
